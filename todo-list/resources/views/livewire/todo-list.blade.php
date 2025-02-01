4ade0274017ccc829cb4bc5ff9c5d5bf9c8c1b03
Joshua Soden <joshuasodenmedia@hotmail.com>
2025-02-01 23:37:06 +0000

    Style todo text, container

--- todo-list/resources/views/livewire/todo-list.blade.php
+++ todo-list/resources/views/livewire/todo-list.blade.php
@@ -2,19 +2,23 @@
     
     <h3>To do:</h3>
     @foreach($todos_incomplete as $todo)
-        <div wire:key="{{$todo->id}}">
+        <div class="todo" wire:key="{{$todo->id}}">
             <p>{{ $todo->title }}</p>
-            <input type="checkbox" wire:click="mark_complete({{ $todo->id }}, {{ true }})"/>
-            <button wire:click="delete_todo({{ $todo->id }})">Delete</a>
+            <div>
+                <input type="checkbox" wire:click="mark_complete({{ $todo->id }}, {{ true }})"/>
+                <button class="delete" wire:click="delete_todo({{ $todo->id }})">Delete</a>
+            </div>
         </div>
     @endforeach
 
     <h3>Already done:</h3>
     @foreach($todos_complete as $todo)
-        <div wire:key="{{$todo->id}}">
+        <div class="todo" wire:key="{{$todo->id}}">
             <p>{{ $todo->title }}</p>
-            <input type="checkbox" checked wire:click="mark_complete({{ $todo->id }}, {{ 0 }})"/>
-            <button wire:click="delete_todo({{ $todo->id }})">Delete</a>
+            <div>
+                <input type="checkbox" checked wire:click="mark_complete({{ $todo->id }}, {{ 0 }})"/>
+                <button class="delete" wire:click="delete_todo({{ $todo->id }})">Delete</a>
+            </div>
         </div>
     @endforeach
 
