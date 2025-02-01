4e90f53ca8cf276be2f5ce1c27b0be31cad91fc7
Joshua Soden <joshuasodenmedia@hotmail.com>
2025-02-01 22:21:47 +0000

    Add, remove todos & save to DB

--- todo-list/resources/views/livewire/todo-list.blade.php
+++ todo-list/resources/views/livewire/todo-list.blade.php
@@ -2,14 +2,18 @@
     
     <h3>To do:</h3>
     @foreach($todos_incomplete as $todo)
-        <p>{{ $todo->title }}</p>
-        <input type="checkbox"/>
+        <div wire:key="{{$todo->id}}">
+            <p>{{ $todo->title }}</p>
+            <input type="checkbox" wire:click="mark_complete({{ $todo->id }}, {{ true }})"/>
+        </div>
     @endforeach
 
     <h2>Already done:</h2>
     @foreach($todos_complete as $todo)
-        <p>{{ $todo->title }}</p>
-        <input type="checkbox" checked/>
+        <div wire:key="{{$todo->id}}">
+            <p>{{ $todo->title }}</p>
+            <input type="checkbox" checked wire:click="mark_complete({{ $todo->id }}, {{ 0 }})"/>
+        </div>
     @endforeach
 
 </div>
