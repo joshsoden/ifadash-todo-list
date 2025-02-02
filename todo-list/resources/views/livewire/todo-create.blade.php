c50f2fc01a31b75d65404ccfce0fbc723dd8f28e
Joshua Soden <joshuasodenmedia@hotmail.com>
2025-02-02 09:51:06 +0000

    Create new item section

--- todo-list/resources/views/livewire/todo-create.blade.php
+++ todo-list/resources/views/livewire/todo-create.blade.php
@@ -1,7 +1,9 @@
 <div>
     <h3>Create new item</h3>
     <form wire:submit="create_todo">
-        <label>Name: </label><input type="text" wire:model="title" />
-        <button type="submit">Create</button>
+        <div class="todo">
+            <input type="text" wire:model="title" />
+            <span type="submit" class="material-symbols-outlined">add_box</span>
+        </dv>
     </form>
 </div>
