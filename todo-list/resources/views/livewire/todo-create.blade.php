00bde2bb0ab250aad5e559b91aa377805ed06d8d
Joshua Soden <joshuasodenmedia@hotmail.com>
2025-02-03 14:22:29 +0000

    Add input placeholder attr

--- todo-list/resources/views/livewire/todo-create.blade.php
+++ todo-list/resources/views/livewire/todo-create.blade.php
@@ -2,7 +2,7 @@
     <h3>Create new item</h3>
     <form wire:submit="create_todo">
         <div class="todo">
-            <input type="text" wire:model="title" />
+            <input placeholder="e.g. 'Fold the laundry'" ="text" wire:model="title" />
             <span type="submit" class="material-symbols-outlined">add_box</span>
         </dv>
     </form>
